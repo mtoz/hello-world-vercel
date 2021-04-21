d17914a789ec697d276b6153516cb914f1e08a7e
mtoz <77736733+mtoz@users.noreply.github.com>
2021-04-21 13:44:59 +0200

    test push index

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,10 @@ export default function IndexPage() {
       <Link href="/about">
         <a>About</a>
       </Link>
+      <h1>Mon texte</h1>
+      <p>Paragraphe</p>
+      <p>Paragraphe numéro 2</p>
+
     </div>
   )
 }
