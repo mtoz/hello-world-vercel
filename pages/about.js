74277fd169f7b4b47d4e23d62715cc8d85b8e199
mtoz <ozturk.metin@hotmail.fr>
2021-04-22 09:40:12 +0200

    test extensions

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,18 @@
-export default function AboutPage() {
-  return <div>About us</div>
+import Link from 'next/link'
+import style from '../styles/about.module.css'
+
+export default function About() {
+  return (
+    <div>
+      Hello World.{' '}
+      <Link href="/">
+        <a>Accueil</a>
+      </Link>
+      <h1 className={style.center}>Page contact</h1>
+      <p>Paragraphe contact</p>
+      <p>Paragraphe numéro 2 page contact</p>
+      <p>Test extensions</p>
+
+    </div>
+  )
 }
