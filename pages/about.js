2a3929324f49846d66a1c57b91df6e3b72bfa50f
mtoz <ozturk.metin@hotmail.fr>
2021-05-02 15:15:56 +0200

    modification de la structure de la page 02/05/21

--- pages/about.js
+++ pages/about.js
@@ -3,26 +3,12 @@ import style from '../styles/about.module.css'
 
 export default function About() {
   return (
-   
-      <div className={style.body}>
 
-        <div className={style.btnContainer}>
+    // Début du code html
+    <div>
 
-          <div className={style.btn}>
-            <a href="#">Read More</a>
-          </div>   
-
-          <div className={style.btn}>
-            <a href="#">Read More</a>
-          </div>
-
-        </div>
-       
-
-        <div className={style.greyBanner}>
-        </div>  
-
-        {/* Début des cards */}
+      {/* Début container cards */}
+      <div className={style.container}>
 
         <div className={style.containCard}>
 
@@ -65,13 +51,65 @@ export default function About() {
 
           </div>
 
+          {/* 4ème */}
+          <div className={style.card}>
+
+            <div className={style.imgBx}>
+            </div>
+
+            <div className={style.content}>
+              <h2>Card one</h2>
+              <p> lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor</p>
+            </div>
+
+          </div>
+
+           {/* 5ème */}
+          <div className={style.card}>
+
+            <div className={style.imgBx}>
+            </div>
+
+            <div className={style.content}>
+              <h2>Card one</h2>
+              <p> lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor</p>
+            </div>
+
+          </div>
+
         </div>
 
-        {/* FIN */}
-
-         
       </div>
+      {/* Fin container des cards */}
 
-     
+
+      {/* Début container bouton */}
+      <div className={style.container}>
+        <div className={style.btnContainer}>
+
+          <div className={style.btn}>
+            <a href="#">Read More</a>
+          </div>
+
+          <div className={style.btn}>
+            <a href="#">Read More</a>
+          </div>
+
+          <div className={style.btn}>
+            <a href="#">Read More</a>
+          </div>
+
+          <div className={style.btn}>
+            <a href="#">Read More</a>
+          </div>
+
+        </div>
+      </div>
+      {/* Fin container boutons */}
+
+    </div>
+    // Fin du code html
+
+
   )
 }
